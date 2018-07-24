bed4bd269fb9947314df9c20b9369e70c7b67948
Georgi Iliev <giliev@vmware.com>
2018-07-24 16:07:56 +0300

    Enable CORS and secure user creation

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,14 @@
 from typing import List, ClassVar
 from sqlalchemy.orm import Session
-from flask import Flask, jsonify, request
-from flask.json import loads
+from flask import Flask, jsonify, request, Response
+from flask_cors import CORS
 from .entities import SessionFactory, engine, Base, User, UserSchema
 from .services import dbservices, UserService
 
 Base.metadata.create_all(engine)
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/api/users')
@@ -20,6 +21,10 @@ def get_users(user_svc: UserService):
 @app.route('/api/users', methods=["POST"])
 @dbservices(user_svc=UserService)
 def post_users(user_svc: UserService):
+    # only allow user creation from localhost
+    if request.remote_addr != "127.0.0.1":
+        return Response(status=403)
+
     data = UserSchema(exclude=('id')).load(request.get_json()).data
     user = user_svc.create_user(data)
     return jsonify(user), 201
